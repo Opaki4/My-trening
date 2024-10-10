8c03fba60020fe701552f61136cd9a9a12f54b35
Aleksandr <132878764+Opaki4@users.noreply.github.com>
2024-10-10 12:42:16 +0300

    Add files via upload

---
+++ module_2_hard.py
@@ -0,0 +1,11 @@
+#x = int(input('Введите число от 3 до 20: '))
+
+x = int(input('Введите число от 3 до 20: '))
+if x < 3 or x > 20:
+    print('отрубить палец дебилу')
+result = ''
+for i in range(1, x):
+    for j in range(1, x):
+        if x % (i+j) == 0 and i < j:
+            result += str(i)+str(j)
+print(result)
